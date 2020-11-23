6881621f0e1cba2b3780f1c4e38be9b5c8a5bb67
abhyuday-tripathi <72795925+abhyuday-tripathi@users.noreply.github.com>
2020-11-23 19:34:52 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,15 @@
+burger = document.querySelector(".burger");
+navbar = document.querySelector(".navbar");
+navList = document.querySelector(".nav-list");
+rightNav = document.querySelector(".rightNav");
+
+burger.addEventListener("click", () => {
+  rightNav.classList.toggle("v-class-resp");
+  navList.classList.toggle("v-class-resp");
+  navbar.classList.toggle("h-nav-resp");
+  navbar.classList.toggle("white");
+});
+
+window.addEventListener("scroll", function () {
+  navbar.classList.toggle("white", window.scrollY > 0);
+});
